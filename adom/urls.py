55df29584eb7b0b1d10c22fd21b6e7cdb75d17d5
kofi <kadukpo@gmail.com>
2023-12-07 15:34:22 +0000

    Your commit message

--- adom/urls.py
+++ adom/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('trigger-action/', views.music_view, name='trigger_action'),
     path('all/', views.index, name='index'),
     path('all/base.html/', views.base, name='base'),
-    path('handle-click/', views.handle_click, name='handle_click'),  # Maps /handle-click/ to handle_click view
+    path('', views.handle_click, name='handle_click'),  # Maps /handle-click/ to handle_click view
     path('previous/', views.previous_view, name='previous_view'),
     path('search/', views.search_hymn, name='search_hymn'),
 
